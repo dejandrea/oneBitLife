e336ac04c85c190fbeb20ee3a4680e992c092eaa
Andréa J S França <dejandreafranca@gmail.com>
2022-12-03 17:34:21 -0300

    estilização Start

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,19 +1,40 @@
 import React from "react";
-import { ScrollView, View, Image, Text } from 'react-native';
+import { ScrollView, View, Image, Text, StyleSheet} from 'react-native';
 
 export default function Start() {
   return (
-    <View>
+    <View style={styles.container}>
       <ScrollView showsVerticalScrollIndicator={false}>
-        <View>
-          <Image source={require("../../assets/icons/logo.png")}>
+        <View style={{alignItems:"center"}}>
+          <Image style={
+            styles.logo
+          } source={require("../../assets/icons/logo3.png")}>
 
           </Image>
-          <Text>
-            Vamos Transformar sua vida {/n} em Jogo, buscando sempre {/n} o melhor nível
+          <Text style={styles.description}>
+            Vamos Transformar sua vida {'\n'} em um Jogo, buscando sempre {'\n'} o melhor nível
           </Text>
         </View>
       </ScrollView>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:'rgba(21,21,21,0.98)'
+  },
+  logo:{
+    width:300,
+    height:60,
+    marginTop:60,
+    marginBottom:20,
+  },
+  description:{
+    color:"#fff",
+    fontSize:20,
+    textAlign:"center",
+    marginVertical:60
+  }
+})
