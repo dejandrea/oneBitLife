6a599f60193fe815afbb7ffab8bb5b10393aef2a
Andréa J S França <dejandreafranca@gmail.com>
2022-12-04 10:20:51 -0300

    AppExplanation

---
+++ src/Pages/AppExplanation/index.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+
+import DefaultButton from "../../Components/Common/DefaultButton";
+
+export default function AppExplanation (){
+  function handleSetShowHome(){
+    console.log("Testando o clique Explain")
+  }
+  return(
+    <View style={styles.container}>
+      <ScrollView>
+        <View style={{alignItems:"center"}}>
+          <Text style={styles.title}>
+            Antes, deixa {'\n'} eu te explicar...
+          </Text>
+
+          <Text style={styles.descriptionCta}>
+            Pronto(a) para subir de nível na vida?
+          </Text>
+          <Text style={styles.description}>
+            Na próxima tela você vai poder escolher {'\n'} seus 4 hábitos de forma individual
+          </Text>
+
+          <DefaultButton 
+            buttonText={'Continuar'}
+            handlePress={handleSetShowHome}
+            width={250}
+            height={50}
+          />
+        </View>
+      </ScrollView>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: 'rgba(21,21,21,0.98)'
+  },
+  title:{
+    fontSize:30,
+    fontWeight:"bold",
+    textAlign:"center",
+    marginVertical:40,
+    color:"#fff"
+  },
+  descriptionCta:{
+    fontSize:16,
+    fontWeight:"bold",
+    color:"#fff",
+    marginTop:20,
+    marginBottom:10
+  },
+  description:{
+    fontSize:16,
+    fontWeight:"bold",
+    color:"#fff",
+    textAlign:"center",
+    marginTop:20,
+    marginBottom:30
+  }
+})
